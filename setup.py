816cca91232be7162a48a18a4cb27f1a88b13dea
Adomas Paltanavi?ius <adomas@shrubberysoft.com>
2009-09-22 02:29:54 +0300

    Include a bit longer description.

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name="yuicompressor",
     description="YUI Compressor packaged for Python",
-    long_description="YUI Compressor archive and entry point.",
+    long_description="YUI Compressor archive bundled inside an egg, "
+                     "with an entry point provided to ease the invocation.",
     version="2.4.2",
     url="",
     license='BSD',
     packages=find_packages(),
     install_requires=[],
-    package_data={
-        '': ["*.jar"]
-    },
-    zip_safe=False,
     entry_points={
         'console_scripts': [
             "yuicompressor = yuicompressor:main"
         ]
-    }
+    },
+    package_data={
+        '': ["*.jar"]
+    },
+    include_package_data=True,
+    zip_safe=False,
 )
 
