8a6025e08a00cc4adcf422e5f3d2c1e5fe11391d
Adomas Paltanavi?ius <adomas.paltanavicius@gmail.com>
2011-04-21 19:32:39 +0300

    Update yuicompressor.jar to 2.4.6

--- yuicompressor/__init__.py
+++ yuicompressor/__init__.py
@@ -2,6 +2,8 @@ import sys
 import os
 from pkg_resources import resource_filename
 
+__version__ = '2.4.6'
+
 
 def get_jar_filename():
     """Return the full path to the YUI Compressor Java archive."""
